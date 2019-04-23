e850e72200746f77f8bfa1dca00dab4373f5bece
Jordynn Otto <j_taylor92@yahoo.com>
2019-04-23 15:54:23 -0700

    html

---
+++ assets/app.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+  // Add smooth scrolling to nav bar links
+  $("#about-me").on('click', function (event) {
+    $('html, body').animate({
+      scrollTop: ($('#about').offset().top)
+    }, 700);
+  });
+  $("#web").on('click', function (event) {
+    $('html, body').animate({
+      scrollTop: ($('#webPage').offset().top)
+    }, 700);
+  });
+  $("#art").on('click', function (event) {
+    $('html, body').animate({
+      scrollTop: ($('#artPage').offset().top)
+    }, 700);
+  });
+  $("#contact").on('click', function(event) {
+    $('html, body').animate({
+      scrollTop: ($('#contactPage').offset().top)
+  },500);
+});
+
+});
+
+// // Activate Carousel
+// // $("#myCarousel").carousel();
+
+// // Enable Carousel Indicators
+// $(".item").click(function(){
+//   $("#myCarousel").carousel(1);
+// });
+
+// // Enable Carousel Controls
+// $(".left").click(function(){
+//   $("#myCarousel").carousel("prev");
+// });
