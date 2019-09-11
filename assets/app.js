01f8e94d0510bc34cb547fcdcc5f8a6f74308844
Jordynn Otto <j_taylor92@yahoo.com>
2019-09-10 22:27:39 -0700

    js and formatting of skills

---
+++ assets/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  $("#about-me").on('click', function (event) {
+    $('html, body').animate({
+      scrollTop: ($('#about').offset().top)
+    }, 500);
+  });
+  $("#web").on('click', function (event) {
+    $('html, body').animate({
+      scrollTop: ($('#webPage').offset().top)
+    }, 500);
+  });
+  $("#art").on('click', function (event) {
+    $('html, body').animate({
+      scrollTop: ($('#artPage').offset().top)
+    }, 500);
+  });
+  $("#contact").on('click', function (event) {
+    $('html, body').animate({
+      scrollTop: ($('#contactPage').offset().top)
+    }, 500);
+  });
+});
